9141546e58c3412a50768fb7727bb1a3e50e4198
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-01 11:30:31 +0530

    Filed Entity class created with annotations

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/entity/FieldEntity.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/entity/FieldEntity.java
@@ -1,4 +1,16 @@
 package lk.ijse.gdse.aad67.greenshadowbackendapi.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "field")
 public class FieldEntity {
+
 }
