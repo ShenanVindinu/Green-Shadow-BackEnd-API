4d61777c25817342762d64f52407ae386f4e6d5e
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-02 00:31:15 +0530

    FieldController created with dummy method

---
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/controller/FieldController.java
@@ -0,0 +1,18 @@
+package lk.ijse.gdse.aad67.greenshadowbackendapi.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/field")
+public class FieldController {
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> addField() {
+        return ResponseEntity.ok().build();
+    }
+
+}
