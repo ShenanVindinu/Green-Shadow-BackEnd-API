8350b28aa3a0fb27eb019b03e2da54a9912b5d85
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-01 14:48:25 +0530

    Enum Datatypes added for properties

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/entity/impl/EquipmentEntity.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/entity/impl/EquipmentEntity.java
@@ -1,9 +1,11 @@
-package lk.ijse.gdse.aad67.greenshadowbackendapi.entity;
+package lk.ijse.gdse.aad67.greenshadowbackendapi.entity.impl;
 
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lk.ijse.gdse.aad67.greenshadowbackendapi.entity.EquipmentStatus;
+import lk.ijse.gdse.aad67.greenshadowbackendapi.entity.EquipmentType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,8 +22,8 @@ public class EquipmentEntity {
     private String equipmentId;
 
     private String equipmentName;
-    private Enum equipmentType;
-    private Enum equipmentStatus;
+    private EquipmentType equipmentType;
+    private EquipmentStatus equipmentStatus;
     private Staff assignedStaffDetails;
     private Field assignedFieldDetails;
 
