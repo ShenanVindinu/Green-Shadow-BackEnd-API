1ff31897e9219423d9490abde3093a69e80bae84
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-01 11:43:26 +0530

    Filed Entity attributes added

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/entity/FieldEntity.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/entity/FieldEntity.java
@@ -2,10 +2,14 @@ package lk.ijse.gdse.aad67.greenshadowbackendapi.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.awt.*;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,5 +19,16 @@ import lombok.NoArgsConstructor;
 public class FieldEntity {
     @Id
     private String fieldCode;
+    private String fieldName;
+    private Point fieldLocation;
+    private Double extentSizeOfTheField;
+
+    private List<Crop> crops;
+    private List<Staff> staff;
+
+    @Lob
+    private String fieldImage1;
+    @Lob
+    private String fieldImage2;
 
 }
