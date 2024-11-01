36e0b656fd753d8556f34318bf04b41ec2b14d60
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-01 13:08:56 +0530

    MonitoringLogEntity added

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/entity/MonitoringLog.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/entity/MonitoringLog.java
@@ -2,10 +2,15 @@ package lk.ijse.gdse.aad67.greenshadowbackendapi.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.lang.reflect.Field;
+import java.util.Date;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,4 +20,14 @@ import lombok.NoArgsConstructor;
 public class MonitoringLog {
     @Id
     private String logCode;
+
+    private Date logDate;
+    private String logDetails;
+
+    @Lob
+    private String observedImage;
+
+    private List<Field> field;
+    private List<Crop> crop;
+    private List<Staff> staff;
 }
