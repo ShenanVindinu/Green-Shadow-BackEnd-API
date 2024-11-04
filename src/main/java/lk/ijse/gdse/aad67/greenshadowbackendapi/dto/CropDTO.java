196a022a275f6e6741dd5b888eb3bfb8f5fc1cf2
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-04 11:59:08 +0530

    DTO properties added

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/dto/CropDTO.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/dto/CropDTO.java
@@ -1,5 +1,7 @@
 package lk.ijse.gdse.aad67.greenshadowbackendapi.dto;
 
+
+import lk.ijse.gdse.aad67.greenshadowbackendapi.entity.impl.FieldEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,4 +10,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class CropDTO {
+
+    private String cropCode;
+    private String cropCommonName;
+    private String cropScientificName;
+    private String cropImage;
+    private String category;
+    private String cropSeason;
+    private FieldEntity field;
+
 }
