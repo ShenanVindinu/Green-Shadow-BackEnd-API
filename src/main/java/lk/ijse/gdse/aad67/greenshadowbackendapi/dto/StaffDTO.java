7f99841af5a171055b5dd8eeffe0c790245463f1
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-04 12:13:49 +0530

    DTO properties added

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/dto/StaffDTO.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/dto/StaffDTO.java
@@ -1,11 +1,35 @@
 package lk.ijse.gdse.aad67.greenshadowbackendapi.dto;
 
+import lk.ijse.gdse.aad67.greenshadowbackendapi.entity.Gender;
+import lk.ijse.gdse.aad67.greenshadowbackendapi.entity.Role;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class StaffDTO {
+
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String designation;
+    private Gender gender;
+    private Date joinedDate;
+    private Date dob;
+    private String addressLine01;
+    private String addressLine02;
+    private String addressLine03;
+    private String addressLine04;
+    private String addressLine05;
+    private String contactNumber;
+    private String email;
+    private Role role;
+    private List<FieldDTO> field;
+    private List<VehicleDTO> vehicle;
+
 }
