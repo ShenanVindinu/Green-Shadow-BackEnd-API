aef39bfac0c60271aadad6684ee4abd1bd64ea3b
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-04 21:41:48 +0530

    Custom Exception Created

---
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/exception/DataPersistException.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse.aad67.greenshadowbackendapi.exception;
+
+public class DataPersistException extends RuntimeException {
+    public DataPersistException(String message) {
+        super(message);
+    }
+}
