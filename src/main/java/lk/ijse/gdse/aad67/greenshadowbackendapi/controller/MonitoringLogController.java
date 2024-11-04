4aec25e870b8e5c5c4d826befc89643af8647293
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-04 20:58:40 +0530

    New Controller Created

---
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/controller/MonitoringLogController.java
@@ -0,0 +1,9 @@
+package lk.ijse.gdse.aad67.greenshadowbackendapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/Mlog")
+public class MonitoringLogController {
+}
