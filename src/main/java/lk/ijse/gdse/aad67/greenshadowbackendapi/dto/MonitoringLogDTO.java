8dfb5f7ddd9c9915beacd6f8839e49358aff2e64
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-04 12:13:44 +0530

    DTO properties added

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/dto/MonitoringLogDTO.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/dto/MonitoringLogDTO.java
@@ -1,11 +1,24 @@
 package lk.ijse.gdse.aad67.greenshadowbackendapi.dto;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class MonitoringLogDTO {
+
+    private String logCode;
+    private Date logDate;
+    private String logDetails;
+    private String observedImage;
+    private List<FieldDTO> field;
+    private List<CropDTO> crop;
+    private List<StaffDTO> staff;
+
 }
