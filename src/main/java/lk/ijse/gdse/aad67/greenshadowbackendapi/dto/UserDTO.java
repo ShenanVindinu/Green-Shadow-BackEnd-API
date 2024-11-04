1ba9008ea049a74dcc769183907428d0b3ea34cc
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-04 12:13:55 +0530

    DTO properties added

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/dto/UserDTO.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/dto/UserDTO.java
@@ -1,5 +1,6 @@
 package lk.ijse.gdse.aad67.greenshadowbackendapi.dto;
 
+import lk.ijse.gdse.aad67.greenshadowbackendapi.entity.Role;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,4 +9,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class UserDTO {
+
+    private String email;
+    private String password;
+    private Role role;
+
 }
