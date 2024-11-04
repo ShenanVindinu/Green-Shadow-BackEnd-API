20e7d5be9e863b7087d7bcac3486309dbb1a4a29
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-04 12:01:54 +0530

    lombok annotations added

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/dto/EquipmentDTO.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/dto/EquipmentDTO.java
@@ -1,4 +1,20 @@
 package lk.ijse.gdse.aad67.greenshadowbackendapi.dto;
 
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lk.ijse.gdse.aad67.greenshadowbackendapi.entity.EquipmentStatus;
+import lk.ijse.gdse.aad67.greenshadowbackendapi.entity.EquipmentType;
+import lk.ijse.gdse.aad67.greenshadowbackendapi.entity.impl.FieldEntity;
+import lk.ijse.gdse.aad67.greenshadowbackendapi.entity.impl.StaffEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class EquipmentDTO {
+
+
+
 }
