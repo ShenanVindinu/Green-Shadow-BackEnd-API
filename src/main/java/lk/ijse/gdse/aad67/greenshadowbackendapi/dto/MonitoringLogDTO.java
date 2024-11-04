c1fdb03102f77c09375340c56a957c3f1af27666
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-04 12:04:04 +0530

    lombok annotations added

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/dto/MonitoringLogDTO.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/dto/MonitoringLogDTO.java
@@ -1,4 +1,11 @@
 package lk.ijse.gdse.aad67.greenshadowbackendapi.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class MonitoringLogDTO {
 }
