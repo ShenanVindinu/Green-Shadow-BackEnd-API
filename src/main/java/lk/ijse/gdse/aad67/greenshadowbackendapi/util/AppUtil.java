9c5721a3fc4b683f1803647690dced24ecf1cbec
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-04 21:37:29 +0530

    AppUtil created

---
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/util/AppUtil.java
@@ -0,0 +1,34 @@
+package lk.ijse.gdse.aad67.greenshadowbackendapi.util;
+
+import java.util.Base64;
+import java.util.UUID;
+
+public class AppUtil {
+    public static String generateUserId(){
+        return "USER-"+UUID.randomUUID();
+    }
+    public static String generateCropId(){
+        return "CROP-"+UUID.randomUUID();
+    }
+    public static String generateEquipmentId(){
+        return "EQUIPMENT-"+UUID.randomUUID();
+    }
+    public static String generateFieldId(){
+        return "FIELD-"+UUID.randomUUID();
+    }
+    public static String generateMonitoringId(){
+        return "MONITORING-"+UUID.randomUUID();
+    }
+    public static String generateStaffId(){
+        return "STAFF-"+UUID.randomUUID();
+    }
+    public static String generateMonitoringLogId(){
+        return "MonitoringLOG-"+UUID.randomUUID();
+    }
+    public static String generateVehicleId(){
+        return "Vehicle-"+UUID.randomUUID();
+    }
+//    public static String profilePicToBase64(byte [] profilePic){
+//       return Base64.getEncoder().encodeToString(profilePic);
+//    }
+}
