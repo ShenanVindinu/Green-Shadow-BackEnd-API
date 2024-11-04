d51a410e78875ce34753b4e5c58d99e71a2c68f0
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-04 21:13:38 +0530

    SuperEntity interface implemented

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/entity/impl/MonitoringLogEntity.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/entity/impl/MonitoringLogEntity.java
@@ -1,6 +1,7 @@
 package lk.ijse.gdse.aad67.greenshadowbackendapi.entity.impl;
 
 import jakarta.persistence.*;
+import lk.ijse.gdse.aad67.greenshadowbackendapi.entity.SuperEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,7 @@ import java.util.List;
 @Data
 @Entity
 @Table(name = "monitering_Log")
-public class MonitoringLogEntity {
+public class MonitoringLogEntity implements SuperEntity {
     @Id
     private String logCode;
 
