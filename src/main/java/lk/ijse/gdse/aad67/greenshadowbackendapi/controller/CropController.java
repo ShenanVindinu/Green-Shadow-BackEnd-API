ed9491e23a2a836e4a52ff8077ffc0381e315cc9
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-10 21:33:38 +0530

    CropSave Method Created

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/controller/CropController.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/controller/CropController.java
@@ -1,9 +1,36 @@
 package lk.ijse.gdse.aad67.greenshadowbackendapi.controller;
 
+import lk.ijse.gdse.aad67.greenshadowbackendapi.dto.CropDTO;
+import lk.ijse.gdse.aad67.greenshadowbackendapi.exception.DataPersistException;
+import lk.ijse.gdse.aad67.greenshadowbackendapi.service.CropService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/crop")
 public class CropController {
+
+    @Autowired
+    private CropService cropService;
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveCrop(@RequestBody CropDTO cropDTO) {
+        try {
+            cropService.saveCrop(cropDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (DataPersistException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 }
