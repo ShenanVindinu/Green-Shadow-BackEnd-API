35b84c1b73423dd42b92a66122567cbcd908c724
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-10 21:33:48 +0530

    CropSave Method Added

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/service/CropService.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/service/CropService.java
@@ -1,4 +1,7 @@
 package lk.ijse.gdse.aad67.greenshadowbackendapi.service;
 
+import lk.ijse.gdse.aad67.greenshadowbackendapi.dto.CropDTO;
+
 public interface CropService {
+    void saveCrop(CropDTO cropDTO);
 }
