db910d185fff2ce93e529ad3d4473025e0f931a2
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-10 21:33:57 +0530

    CropSave Method Implemented

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/service/impl/CropServiceIMPL.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/service/impl/CropServiceIMPL.java
@@ -1,8 +1,13 @@
 package lk.ijse.gdse.aad67.greenshadowbackendapi.service.impl;
 
+import lk.ijse.gdse.aad67.greenshadowbackendapi.dto.CropDTO;
 import lk.ijse.gdse.aad67.greenshadowbackendapi.service.CropService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CropServiceIMPL implements CropService {
+    @Override
+    public void saveCrop(CropDTO cropDTO) {
+
+    }
 }
