509ea4be3179efc6d3b0185a00ddb8acd3d51394
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-11 15:07:11 +0530

    Add transactional support and logging to CropServiceIMPL
    This commit introduces `@Transactional` to the `CropServiceIMPL` class to ensure proper transaction management. It also adds logging capabilities and integrates dependency injection for `CropDAO` and `Mapping` utilities.

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/service/impl/CropServiceIMPL.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/service/impl/CropServiceIMPL.java
@@ -1,13 +1,31 @@
 package lk.ijse.gdse.aad67.greenshadowbackendapi.service.impl;
 
+import jakarta.transaction.Transactional;
+import lk.ijse.gdse.aad67.greenshadowbackendapi.dao.CropDAO;
 import lk.ijse.gdse.aad67.greenshadowbackendapi.dto.CropDTO;
+import lk.ijse.gdse.aad67.greenshadowbackendapi.entity.impl.CropEntity;
 import lk.ijse.gdse.aad67.greenshadowbackendapi.service.CropService;
+import lk.ijse.gdse.aad67.greenshadowbackendapi.util.Mapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
+@Transactional
 public class CropServiceIMPL implements CropService {
+
+    private final CropDAO cropDAO;
+    private final Mapping mapping;
+    private final Logger logger = LoggerFactory.getLogger(CropServiceIMPL.class);
+
+    public CropServiceIMPL(CropDAO cropDAO, Mapping mapping) {
+        this.cropDAO = cropDAO;
+        this.mapping = mapping;
+    }
+
     @Override
     public void saveCrop(CropDTO cropDTO) {
-
+        logger.info(cropDTO.getField());
+        CropEntity cropEntity = cropDAO.save(mapping.toCropEntity(cropDTO));
     }
 }
