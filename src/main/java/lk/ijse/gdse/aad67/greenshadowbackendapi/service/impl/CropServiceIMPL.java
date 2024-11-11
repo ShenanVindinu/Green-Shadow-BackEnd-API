41867ff86057f749c9ccda664a39f74ef761ea0f
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-11 18:19:11 +0530

    Remove unused import from CropServiceIMPL
    Eliminated the unnecessary import statement for CropEntity to clean up the code. Adjusted the `saveCrop` method to directly use the mapping without assigning it to a variable.

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/service/impl/CropServiceIMPL.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/service/impl/CropServiceIMPL.java
@@ -3,7 +3,6 @@ package lk.ijse.gdse.aad67.greenshadowbackendapi.service.impl;
 import jakarta.transaction.Transactional;
 import lk.ijse.gdse.aad67.greenshadowbackendapi.dao.CropDAO;
 import lk.ijse.gdse.aad67.greenshadowbackendapi.dto.CropDTO;
-import lk.ijse.gdse.aad67.greenshadowbackendapi.entity.impl.CropEntity;
 import lk.ijse.gdse.aad67.greenshadowbackendapi.service.CropService;
 import lk.ijse.gdse.aad67.greenshadowbackendapi.util.Mapping;
 import org.slf4j.Logger;
@@ -26,6 +25,6 @@ public class CropServiceIMPL implements CropService {
     @Override
     public void saveCrop(CropDTO cropDTO) {
         logger.info(cropDTO.getField());
-        CropEntity cropEntity = cropDAO.save(mapping.toCropEntity(cropDTO));
+        cropDAO.save(mapping.toCropEntity(cropDTO));
     }
 }
