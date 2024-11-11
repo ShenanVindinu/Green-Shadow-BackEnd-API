64b361b2534be724618b02231e4d44cdfe1f8313
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-11 17:36:52 +0530

    Add @Column annotation to cropCode field
    This change ensures the cropCode field is correctly mapped to the database column named "cropCode". It prevents potential issues related to ORM mapping and enhances code clarity on the database field correspondence.

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/entity/impl/VehicleEntity.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/entity/impl/VehicleEntity.java
@@ -14,6 +14,7 @@ import lombok.NoArgsConstructor;
 public class VehicleEntity implements SuperEntity {
 
     @Id
+    @Column(name = "vehicleCode")
     private String vehicleCode;
 
     private String licensePlateNumber;
