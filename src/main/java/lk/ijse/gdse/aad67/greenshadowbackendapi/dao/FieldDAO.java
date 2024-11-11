4677a34d7f13861cb75dec8aca5650dba40719c7
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-11 20:30:54 +0530

    Change FieldDAO ID type to String
    Updated the FieldDAO interface to use String instead of Integer for the ID type. This modification ensures compatibility with databases where the ID is represented as a string, potentially increasing flexibility.

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/dao/FieldDAO.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/dao/FieldDAO.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FieldDAO extends JpaRepository<FieldEntity, Integer> {
+public interface FieldDAO extends JpaRepository<FieldEntity, String> {
 }
