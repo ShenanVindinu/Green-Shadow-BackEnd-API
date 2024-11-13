c0625668ba1659f09226f171ce882ecbb0585538
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-13 23:34:46 +0530

    Add methods to update and retrieve all crops
    Introduce `getAllCrops` and `updateCrop` methods in `CropService` to facilitate retrieving all crop details and updating existing crops. This extends the service's functionality to cover more CRUD operations.

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/service/CropService.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/service/CropService.java
@@ -2,8 +2,14 @@ package lk.ijse.gdse.aad67.greenshadowbackendapi.service;
 
 import lk.ijse.gdse.aad67.greenshadowbackendapi.dto.CropDTO;
 
+import java.util.List;
+
 public interface CropService {
     void saveCrop(CropDTO cropDTO);
 
     void deleteCrop(String cropCode);
+
+    List<CropDTO> getAllCrops();
+
+    void updateCrop(String cropCode, CropDTO updatedCropDto);
 }
