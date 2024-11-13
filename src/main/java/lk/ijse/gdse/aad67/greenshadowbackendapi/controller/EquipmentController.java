3d9aeecf334bb01cf4133db6c063966305a2a644
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-14 01:00:31 +0530

    Add saveEquipment method in EquipmentController
    Introduced a new POST endpoint to save equipment data using EquipmentService. The method generates a new equipment ID, handles success and various error scenarios with appropriate HTTP status codes.

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/controller/EquipmentController.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/controller/EquipmentController.java
@@ -1,10 +1,13 @@
 package lk.ijse.gdse.aad67.greenshadowbackendapi.controller;
 
+import lk.ijse.gdse.aad67.greenshadowbackendapi.dao.EquipmentDAO;
 import lk.ijse.gdse.aad67.greenshadowbackendapi.dto.EquipmentDTO;
 import lk.ijse.gdse.aad67.greenshadowbackendapi.exception.DataPersistException;
+import lk.ijse.gdse.aad67.greenshadowbackendapi.service.EquipmentService;
 import lk.ijse.gdse.aad67.greenshadowbackendapi.util.AppUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
@@ -18,7 +21,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/equipment")
 public class EquipmentController {
 
+    private EquipmentService equipmentService;
+
+    @Autowired
+    public EquipmentController(EquipmentService equipmentService) {
+        this.equipmentService = equipmentService;
+    }
+
     private static final Logger logger = LoggerFactory.getLogger(EquipmentController.class);
 
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveEquipment(@RequestBody EquipmentDTO equipmentDTO) {
+        try {
+            equipmentDTO.setEquipmentId(AppUtil.generateEquipmentId());
+            equipmentService.saveEquipment(equipmentDTO);
+            logger.info("Equipment Save Success");
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (DataPersistException e) {
+            logger.error("Equipment Not Saved",e);
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            logger.error("Internal Server Error",e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
 
+    }
 }
