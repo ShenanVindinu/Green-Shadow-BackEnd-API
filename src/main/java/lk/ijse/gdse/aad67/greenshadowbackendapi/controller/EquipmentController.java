a87b6c694535cfaca320535e754147315510d3b4
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-14 00:12:46 +0530

    Add imports and logger to EquipmentController
    Introduced necessary imports including DTOs, exception handling, and HTTP utilities to the EquipmentController. Also added a logger for better debugging and logging purposes.

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/controller/EquipmentController.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/controller/EquipmentController.java
@@ -1,9 +1,24 @@
 package lk.ijse.gdse.aad67.greenshadowbackendapi.controller;
 
+import lk.ijse.gdse.aad67.greenshadowbackendapi.dto.EquipmentDTO;
+import lk.ijse.gdse.aad67.greenshadowbackendapi.exception.DataPersistException;
+import lk.ijse.gdse.aad67.greenshadowbackendapi.util.AppUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/equipment")
 public class EquipmentController {
+
+    private static final Logger logger = LoggerFactory.getLogger(EquipmentController.class);
+
+
 }
