a426774d22e680663d641e2bfe0f3b2eda7d57a8
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-14 01:00:40 +0530

    Add saveEquipment method to EquipmentService interface
    This addition allows the application to save equipment details by accepting an EquipmentDTO object. It extends the functionality of the EquipmentService interface, enabling more comprehensive equipment management.

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/service/EquipmentService.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/service/EquipmentService.java
@@ -1,4 +1,7 @@
 package lk.ijse.gdse.aad67.greenshadowbackendapi.service;
 
+import lk.ijse.gdse.aad67.greenshadowbackendapi.dto.EquipmentDTO;
+
 public interface EquipmentService {
+    void saveEquipment(EquipmentDTO equipmentDTO);
 }
