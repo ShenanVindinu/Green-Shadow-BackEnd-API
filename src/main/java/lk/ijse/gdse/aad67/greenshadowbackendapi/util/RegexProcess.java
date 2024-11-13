9652b61522f0c9bbeff790993583ac4da6561e04
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-13 22:18:55 +0530

    Add RegexProcess class for crop ID validation
    Implemented a new utility class, RegexProcess, which includes a method to validate crop IDs using a specific regex pattern. This addition enhances the robustness of crop ID handling by ensuring IDs adhere to the expected format.

---
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/util/RegexProcess.java
@@ -0,0 +1,11 @@
+package lk.ijse.gdse.aad67.greenshadowbackendapi.util;
+
+import java.util.regex.Pattern;
+
+public class RegexProcess {
+    public static boolean cropIdMatcher(String cropId) {
+        String regexForUserID = "^CROP-[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$";
+        Pattern pattern = Pattern.compile(regexForUserID);
+        return pattern.matcher(cropId).matches();
+    }
+}
