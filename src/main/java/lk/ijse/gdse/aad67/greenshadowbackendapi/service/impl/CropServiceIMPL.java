f42c9f2ed097bb1ecbf298b06e13b57824c66a29
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-13 22:19:06 +0530

    Add deleteCrop method to CropServiceIMPL
    Implemented a new method to delete crops by crop code. The method checks if the crop exists and throws a CropNotFoundException if not found, ensuring proper error handling.

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/service/impl/CropServiceIMPL.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/service/impl/CropServiceIMPL.java
@@ -3,11 +3,15 @@ package lk.ijse.gdse.aad67.greenshadowbackendapi.service.impl;
 import jakarta.transaction.Transactional;
 import lk.ijse.gdse.aad67.greenshadowbackendapi.dao.CropDAO;
 import lk.ijse.gdse.aad67.greenshadowbackendapi.dto.CropDTO;
+import lk.ijse.gdse.aad67.greenshadowbackendapi.entity.impl.CropEntity;
+import lk.ijse.gdse.aad67.greenshadowbackendapi.exception.CropNotFoundException;
 import lk.ijse.gdse.aad67.greenshadowbackendapi.service.CropService;
 import lk.ijse.gdse.aad67.greenshadowbackendapi.util.Mapping;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -26,5 +30,15 @@ public class CropServiceIMPL implements CropService {
     public void saveCrop(CropDTO cropDTO) {
         logger.info(cropDTO.getFieldCode());
         cropDAO.save(mapping.toCropEntity(cropDTO));
+    }
+
+    @Override
+    public void deleteCrop(String cropCode) {
+        Optional<CropEntity> findCrop = cropDAO.findById(cropCode);
+        if(findCrop.isEmpty()) {
+            throw new CropNotFoundException("Crop Not Found");
+        } else {
+            cropDAO.deleteById(cropCode);
+        }
     }
 }
