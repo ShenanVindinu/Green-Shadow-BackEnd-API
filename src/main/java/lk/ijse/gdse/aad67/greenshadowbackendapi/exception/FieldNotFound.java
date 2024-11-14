792425d8ddd929abfb6f34f0e02b5213f1908af7
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-15 01:05:19 +0530

    New FieldNotFound Exception Created

---
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/exception/FieldNotFound.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse.aad67.greenshadowbackendapi.exception;
+
+public class FieldNotFound extends RuntimeException {
+    public FieldNotFound(String message) {
+        super(message);
+    }
+}
