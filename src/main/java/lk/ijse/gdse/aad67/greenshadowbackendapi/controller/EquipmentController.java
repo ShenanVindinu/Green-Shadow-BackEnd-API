bb735afcfe3482f675c409b337d9854200da27d6
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-14 22:58:13 +0530

    get all Equipment method created

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/controller/EquipmentController.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/controller/EquipmentController.java
@@ -13,6 +13,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/v1/equipment")
 public class EquipmentController {
@@ -41,6 +43,11 @@ public class EquipmentController {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<EquipmentDTO> getAllEquipment() {
+            return equipmentService.getAllEquipment();
     }
 
     @DeleteMapping(value = "{equipmentId}")
