d18494984d4c0774fa645560589f28d93a820656
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-15 00:55:09 +0530

    fieldIdMatcher method added cropIdMatcher method inverted

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/util/RegexProcess.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/util/RegexProcess.java
@@ -6,12 +6,18 @@ public class RegexProcess {
     public static boolean cropIdMatcher(String cropId) {
         String regexForUserID = "^CROP-[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$";
         Pattern pattern = Pattern.compile(regexForUserID);
-        return pattern.matcher(cropId).matches();
+        return !pattern.matcher(cropId).matches();
     }
 
     public static boolean equipmentIdMatcher(String equipmentId) {
         String regexForUserID = "^EQUIPMENT-[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$";
         Pattern pattern = Pattern.compile(regexForUserID);
         return pattern.matcher(equipmentId).matches();
+    }
+
+    public static boolean fieldIdMatcher(String fieldId) {
+        String regexForUserID = "^FIELD-[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$";
+        Pattern pattern = Pattern.compile(regexForUserID);
+        return pattern.matcher(fieldId).matches();
     }
 }
