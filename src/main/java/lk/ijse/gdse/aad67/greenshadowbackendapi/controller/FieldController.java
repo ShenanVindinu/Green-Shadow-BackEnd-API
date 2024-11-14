766d15215227f4c39493032f260f92daf4f2521c
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-15 00:45:22 +0530

    get all method created

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/controller/FieldController.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/controller/FieldController.java
@@ -10,10 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/field")
@@ -64,6 +63,11 @@ public class FieldController {
             logger.error(e.getMessage());
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<FieldDTO> getAllFields() {
+        return fieldService.getAllFields();
     }
 
 }
