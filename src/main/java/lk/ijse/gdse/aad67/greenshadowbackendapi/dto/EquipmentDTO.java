8c3b2b3c3a3660746bb58fba5c3d6b196e00dfcb
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-14 14:58:23 +0530

    DTO properties updated

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/dto/EquipmentDTO.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/dto/EquipmentDTO.java
@@ -1,7 +1,11 @@
 package lk.ijse.gdse.aad67.greenshadowbackendapi.dto;
 
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lk.ijse.gdse.aad67.greenshadowbackendapi.entity.EquipmentStatus;
 import lk.ijse.gdse.aad67.greenshadowbackendapi.entity.EquipmentType;
+import lk.ijse.gdse.aad67.greenshadowbackendapi.entity.impl.FieldEntity;
+import lk.ijse.gdse.aad67.greenshadowbackendapi.entity.impl.StaffEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,7 +19,7 @@ public class EquipmentDTO {
     private String equipmentName;
     private EquipmentType equipmentType;
     private EquipmentStatus equipmentStatus;
-    private String assignedStaffDetails;
-    private String assignedFieldDetails;
+    private String staffId;
+    private String fieldId;
 
 }
