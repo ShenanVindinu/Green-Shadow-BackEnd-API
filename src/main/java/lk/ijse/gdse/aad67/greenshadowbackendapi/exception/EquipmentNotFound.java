c48434e59bf52fec4387adc0ecc7f2738b1844c5
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-14 21:17:00 +0530

    EquipmentNotFound Exception created

---
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/exception/EquipmentNotFound.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse.aad67.greenshadowbackendapi.exception;
+
+public class EquipmentNotFound extends RuntimeException {
+    public EquipmentNotFound(String message) {
+        super(message);
+    }
+}
