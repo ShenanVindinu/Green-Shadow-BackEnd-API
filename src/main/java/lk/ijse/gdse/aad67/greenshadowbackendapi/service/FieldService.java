0e9b81c3d870e71ac023895b49bfd8de5563bb8c
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-15 00:26:42 +0530

    FieldService save method implemented

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/service/FieldService.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/service/FieldService.java
@@ -1,4 +1,7 @@
 package lk.ijse.gdse.aad67.greenshadowbackendapi.service;
 
+import lk.ijse.gdse.aad67.greenshadowbackendapi.dto.FieldDTO;
+
 public interface FieldService {
+    void saveField(FieldDTO fieldDTO);
 }
