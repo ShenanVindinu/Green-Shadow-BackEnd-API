ddb975dc5db6ed37e443e940093df28b7ef93dc0
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-14 22:58:26 +0530

    get all Equipment method implemented

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/service/EquipmentService.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/service/EquipmentService.java
@@ -2,8 +2,12 @@ package lk.ijse.gdse.aad67.greenshadowbackendapi.service;
 
 import lk.ijse.gdse.aad67.greenshadowbackendapi.dto.EquipmentDTO;
 
+import java.util.List;
+
 public interface EquipmentService {
     void saveEquipment(EquipmentDTO equipmentDTO);
 
     void deleteEquipment(String equipmentId);
+
+    List<EquipmentDTO> getAllEquipment();
 }
