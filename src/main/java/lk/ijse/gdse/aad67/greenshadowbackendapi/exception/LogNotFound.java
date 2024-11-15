fa07b60e897afde199e6b9c4ec877005d0087383
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-15 17:26:52 +0530

    new Exception Created

---
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/exception/LogNotFound.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse.aad67.greenshadowbackendapi.exception;
+
+public class LogNotFound extends RuntimeException {
+    public LogNotFound(String message) {
+        super(message);
+    }
+}
