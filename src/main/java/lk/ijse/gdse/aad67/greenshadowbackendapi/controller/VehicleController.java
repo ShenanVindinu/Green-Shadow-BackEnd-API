8440567c59514d01d1f9205346a8580195078052
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-16 03:14:15 +0530

    GetMapping And PutMapping Added

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/controller/VehicleController.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/controller/VehicleController.java
@@ -2,19 +2,20 @@ package lk.ijse.gdse.aad67.greenshadowbackendapi.controller;
 
 import lk.ijse.gdse.aad67.greenshadowbackendapi.dto.VehicleDTO;
 import lk.ijse.gdse.aad67.greenshadowbackendapi.exception.DataPersistException;
+import lk.ijse.gdse.aad67.greenshadowbackendapi.exception.VehicleNotFoundException;
 import lk.ijse.gdse.aad67.greenshadowbackendapi.service.VehicleService;
+import lk.ijse.gdse.aad67.greenshadowbackendapi.util.Mapping;
+import lk.ijse.gdse.aad67.greenshadowbackendapi.util.RegexProcess;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import javax.xml.crypto.Data;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/vehicle")
@@ -24,7 +25,7 @@ public class VehicleController {
     private static final Logger logger = LoggerFactory.getLogger(VehicleController.class);
 
     @Autowired
-    public VehicleController(VehicleService vehicleService) {
+    public VehicleController(VehicleService vehicleService, Mapping mapping) {
         this.vehicleService = vehicleService;
     }
 
@@ -42,6 +43,36 @@ public class VehicleController {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    @GetMapping
+    public List<VehicleDTO> getAllVehicles() {
+        return vehicleService.getAllVehicles();
+    }
+
+    @PutMapping(value = "{vehicleId}",consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> updateVehicle(@PathVariable("vehicleId") String vehicleId,
+                                              @RequestBody VehicleDTO vehicleDTO
+    )
+    {
+        try {
+            if (!RegexProcess.vehicleIdMatcher(vehicleId) || vehicleId == null) {
+                logger.warn("Vehicle id is not valid");
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+            vehicleService.updateVehicle(vehicleId,vehicleDTO);
+            logger.info("Vehicle updated");
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (VehicleNotFoundException vehicleNotFoundException) {
+            logger.error(vehicleNotFoundException.getMessage());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+
 
 
 
