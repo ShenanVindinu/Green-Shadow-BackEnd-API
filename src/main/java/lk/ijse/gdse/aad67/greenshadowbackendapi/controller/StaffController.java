ff157ef95689b3ba3da66de6e515bcbd8f42ba5e
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-16 00:21:59 +0530

    postMapping added for staffMember Save

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/controller/StaffController.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/controller/StaffController.java
@@ -1,9 +1,45 @@
 package lk.ijse.gdse.aad67.greenshadowbackendapi.controller;
 
+import lk.ijse.gdse.aad67.greenshadowbackendapi.dto.StaffDTO;
+import lk.ijse.gdse.aad67.greenshadowbackendapi.entity.impl.StaffEntity;
+import lk.ijse.gdse.aad67.greenshadowbackendapi.exception.DataPersistException;
+import lk.ijse.gdse.aad67.greenshadowbackendapi.service.StaffService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/staff")
 public class StaffController {
+
+    private final StaffService staffService;
+    private static final Logger logger = LoggerFactory.getLogger(StaffController.class);
+
+    @Autowired
+    public StaffController(StaffService staffService) {
+        this.staffService = staffService;
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveStaffMember(@RequestBody StaffDTO staffDTO) {
+        try {
+            staffService.saveStaffMember(staffDTO);
+            logger.info("staff saved");
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (DataPersistException e) {
+            logger.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 }
