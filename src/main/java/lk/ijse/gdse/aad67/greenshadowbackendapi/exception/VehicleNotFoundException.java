85a40d7183047edcd7e7abd4ad8cb3531d11a410
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-16 03:13:12 +0530

    new VehicleNotFoundException Created

---
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/exception/VehicleNotFoundException.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse.aad67.greenshadowbackendapi.exception;
+
+public class VehicleNotFoundException extends RuntimeException {
+    public VehicleNotFoundException(String message) {
+        super(message);
+    }
+}
