89fce2b45860bacc47b2629b2fc70032517a28d8
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-15 23:18:51 +0530

    new Exception Created

---
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/exception/MonitoringLogNotFoundException.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse.aad67.greenshadowbackendapi.exception;
+
+public class MonitoringLogNotFoundException extends RuntimeException {
+    public MonitoringLogNotFoundException(String message) {
+        super(message);
+    }
+}
