96565b6a177e7a766401ffcc04b4e7d1078921d3
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-16 03:14:44 +0530

    get and update methods implemented

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/service/VehicleService.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/service/VehicleService.java
@@ -2,6 +2,12 @@ package lk.ijse.gdse.aad67.greenshadowbackendapi.service;
 
 import lk.ijse.gdse.aad67.greenshadowbackendapi.dto.VehicleDTO;
 
+import java.util.List;
+
 public interface VehicleService {
     void saveVehicle(VehicleDTO vehicleDTO);
+
+    List<VehicleDTO> getAllVehicles();
+
+    void updateVehicle(String vehicleId, VehicleDTO vehicleDTO);
 }
