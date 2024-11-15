8e99a482268f01e6ad200b6e359d0bea07f8261d
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-16 00:22:15 +0530

    save method implemented

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/service/StaffService.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/service/StaffService.java
@@ -1,4 +1,7 @@
 package lk.ijse.gdse.aad67.greenshadowbackendapi.service;
 
+import lk.ijse.gdse.aad67.greenshadowbackendapi.dto.StaffDTO;
+
 public interface StaffService {
+    void saveStaffMember(StaffDTO staffDTO);
 }
