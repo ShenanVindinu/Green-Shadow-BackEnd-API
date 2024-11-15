146b499089a1717a3a813335f6ab13dc03a5a4db
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-16 03:14:55 +0530

    get and update methods added

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/service/impl/VehicleServiceIMPL.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/service/impl/VehicleServiceIMPL.java
@@ -2,6 +2,8 @@ package lk.ijse.gdse.aad67.greenshadowbackendapi.service.impl;
 
 import lk.ijse.gdse.aad67.greenshadowbackendapi.dao.VehicleDAO;
 import lk.ijse.gdse.aad67.greenshadowbackendapi.dto.VehicleDTO;
+import lk.ijse.gdse.aad67.greenshadowbackendapi.entity.impl.VehicleEntity;
+import lk.ijse.gdse.aad67.greenshadowbackendapi.exception.VehicleNotFoundException;
 import lk.ijse.gdse.aad67.greenshadowbackendapi.service.VehicleService;
 import lk.ijse.gdse.aad67.greenshadowbackendapi.util.AppUtil;
 import lk.ijse.gdse.aad67.greenshadowbackendapi.util.Mapping;
@@ -9,6 +11,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class VehicleServiceIMPL implements VehicleService {
@@ -27,5 +32,30 @@ public class VehicleServiceIMPL implements VehicleService {
     public void saveVehicle(VehicleDTO vehicleDTO) {
         vehicleDTO.setVehicleId(AppUtil.generateVehicleId());
         vehicleDAO.save(mapping.toVehicleEntity(vehicleDTO));
+    }
+
+    @Override
+    public List<VehicleDTO> getAllVehicles() {
+        return mapping.asVehicleEntity(vehicleDAO.findAll());
+    }
+
+    @Override
+    public void updateVehicle(String vehicleId, VehicleDTO updatedVehicleDTO) {
+        Optional<VehicleEntity> optionalVehicleEntity = vehicleDAO.findById(vehicleId);
+        if (optionalVehicleEntity.isPresent()) {
+            VehicleEntity vehicle = optionalVehicleEntity.get();
+
+            boolean isChanged = !vehicleId.equals(updatedVehicleDTO.getVehicleId());
+
+            if (isChanged) {
+                vehicleDAO.delete(vehicle);
+            }
+            updatedVehicleDTO.setVehicleId(vehicleId);
+            VehicleEntity newVehicleEntity = mapping.toVehicleEntity(updatedVehicleDTO);
+            vehicleDAO.save(newVehicleEntity);
+
+        } else {
+            throw new VehicleNotFoundException(vehicleId);
+        }
     }
 }
