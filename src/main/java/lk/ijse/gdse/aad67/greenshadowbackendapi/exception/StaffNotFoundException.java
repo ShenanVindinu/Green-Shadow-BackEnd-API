6df34936b768b27584a6a14dda612baf537c84c0
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-16 00:50:03 +0530

    new StaffNotFoundException created

---
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/exception/StaffNotFoundException.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse.aad67.greenshadowbackendapi.exception;
+
+public class StaffNotFoundException extends RuntimeException {
+    public StaffNotFoundException(String message) {
+        super(message);
+    }
+}
