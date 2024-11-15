01a25a81ee58188be8b12c92ab345840f6de3b70
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-15 23:20:57 +0530

    update log method created

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/controller/MonitoringLogController.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/controller/MonitoringLogController.java
@@ -3,6 +3,7 @@ package lk.ijse.gdse.aad67.greenshadowbackendapi.controller;
 import lk.ijse.gdse.aad67.greenshadowbackendapi.dto.MonitoringLogDTO;
 import lk.ijse.gdse.aad67.greenshadowbackendapi.exception.DataPersistException;
 import lk.ijse.gdse.aad67.greenshadowbackendapi.exception.LogNotFound;
+import lk.ijse.gdse.aad67.greenshadowbackendapi.exception.MonitoringLogNotFoundException;
 import lk.ijse.gdse.aad67.greenshadowbackendapi.service.MonitoringLogService;
 import lk.ijse.gdse.aad67.greenshadowbackendapi.util.AppUtil;
 import lk.ijse.gdse.aad67.greenshadowbackendapi.util.RegexProcess;
@@ -77,6 +78,42 @@ public class MonitoringLogController {
     @GetMapping
     public List<MonitoringLogDTO> getAllLogs() {
         return monitoringLogService.getAllLogs();
+    }
+
+    @PutMapping(value = "{logId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> updateLog(@PathVariable String logId,
+                                          @RequestPart("logDetails") String logDetails,
+                                          @RequestPart("observedImage") String observedImage,
+                                          @RequestPart("logDate") String logDate,
+                                          @RequestPart("field") String field,
+                                          @RequestPart("staff") String staff
+    ) {
+        String base64ObservedImage = "";
+        try {
+            base64ObservedImage = AppUtil.PicToBase64(observedImage.getBytes());
+            MonitoringLogDTO monitoringLogDTO = new MonitoringLogDTO();
+            monitoringLogDTO.setLogId(logId);
+            monitoringLogDTO.setLogDate(logDate);
+            monitoringLogDTO.setLogDetails(logDetails);
+            monitoringLogDTO.setObservedImage(base64ObservedImage);
+            monitoringLogDTO.setField(field);
+            monitoringLogDTO.setStaff(staff);
+
+            if (!RegexProcess.logIdMatcher(logId) || logId == null) {
+                logger.warn("Log id is not valid");
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+            monitoringLogService.updateLog(logId, monitoringLogDTO);
+            logger.info("Update log successful");
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (MonitoringLogNotFoundException e) {
+            logger.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 
 }
