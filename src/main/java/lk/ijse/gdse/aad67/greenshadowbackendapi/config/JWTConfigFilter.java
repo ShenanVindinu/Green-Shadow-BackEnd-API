e58673f60c0c8974a0ad1015f1a6df8d143d7b7e
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-25 18:36:12 +0530

    @Configuration @RequiredArgsConstructor added

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/config/JWTConfigFilter.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/config/JWTConfigFilter.java
@@ -4,10 +4,14 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Configuration
+@RequiredArgsConstructor
 public class JWTConfigFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
