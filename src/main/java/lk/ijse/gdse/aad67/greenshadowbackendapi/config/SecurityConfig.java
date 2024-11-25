db359ac217bef4c1f2a561b4139021b68bdd6899
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-25 18:36:06 +0530

    @Configuration @RequiredArgsConstructor added

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/config/SecurityConfig.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/config/SecurityConfig.java
@@ -1,4 +1,9 @@
 package lk.ijse.gdse.aad67.greenshadowbackendapi.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 }
