812cdc41ade44e61e4fcd23cff4b26c92edf74fe
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-28 21:56:30 +0530

    UserNotFoundException.java created

---
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse.aad67.greenshadowbackendapi.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
