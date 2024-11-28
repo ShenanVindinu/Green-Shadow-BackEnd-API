8a40096b2f03a1ccb703ad5c70217a06f7b5d715
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-28 22:48:37 +0530

    userdetails service implementation

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/service/impl/UserServiceIMPL.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/service/impl/UserServiceIMPL.java
@@ -1,8 +1,27 @@
 package lk.ijse.gdse.aad67.greenshadowbackendapi.service.impl;
 
+import lk.ijse.gdse.aad67.greenshadowbackendapi.dao.UserDAO;
+import lk.ijse.gdse.aad67.greenshadowbackendapi.exception.UserNotFoundException;
 import lk.ijse.gdse.aad67.greenshadowbackendapi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceIMPL implements UserService {
+
+    private final UserDAO userDAO;
+
+    @Autowired
+    public UserServiceIMPL(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    @Override
+    public UserDetailsService userDetailsService() {
+        return userName ->
+                userDAO.findByEmail(userName)
+                        .orElseThrow(()-> new UserNotFoundException("User Not Found"));
+    }
+
 }
