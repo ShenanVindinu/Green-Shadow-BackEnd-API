e8a3b41bbc1a81bb1026e18fb5b8e6913ea099f3
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-28 22:48:11 +0530

    findByEmail Custom method signature added for repo

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/dao/UserDAO.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/dao/UserDAO.java
@@ -3,6 +3,8 @@ package lk.ijse.gdse.aad67.greenshadowbackendapi.dao;
 import lk.ijse.gdse.aad67.greenshadowbackendapi.entity.impl.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserDAO extends JpaRepository<UserEntity, String> {
+import java.util.Optional;
 
+public interface UserDAO extends JpaRepository<UserEntity, String> {
+    Optional<UserEntity> findByEmail(String email);
 }
