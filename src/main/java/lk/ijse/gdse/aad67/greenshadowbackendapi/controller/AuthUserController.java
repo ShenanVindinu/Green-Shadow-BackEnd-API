dd80e949c09a48d1ccfe1851622b069fea7d53bf
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-30 23:02:09 +0530

    update and save(signup) implemented signin and refresh token added

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/controller/AuthUserController.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/controller/AuthUserController.java
@@ -1,11 +1,76 @@
 package lk.ijse.gdse.aad67.greenshadowbackendapi.controller;
 
+import lk.ijse.gdse.aad67.greenshadowbackendapi.dto.UserDTO;
+import lk.ijse.gdse.aad67.greenshadowbackendapi.entity.Role;
+import lk.ijse.gdse.aad67.greenshadowbackendapi.exception.DataPersistException;
+import lk.ijse.gdse.aad67.greenshadowbackendapi.secure.JWTAuthResponse;
+import lk.ijse.gdse.aad67.greenshadowbackendapi.secure.SignIn;
+import lk.ijse.gdse.aad67.greenshadowbackendapi.service.AuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
 @RequestMapping("api/v1/auth")
+@RestController
 @RequiredArgsConstructor
 public class AuthUserController {
+
+    private final AuthService authService;
+    private final PasswordEncoder passwordEncoder;
+
+    @PostMapping(value = "signup", consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<JWTAuthResponse> saveUser(
+        @RequestPart("email") String email,
+        @RequestPart("password") String password
+    ) {
+        try {
+            UserDTO buildUserDTO = new UserDTO();
+            buildUserDTO.setEmail(email);
+            buildUserDTO.setPassword(passwordEncoder.encode(password));
+            return ResponseEntity.ok(authService.signUp(buildUserDTO));
+        } catch (DataPersistException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping(value = "userUpdate", consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<JWTAuthResponse> updateUser(
+        @RequestPart("email") String email,
+        @RequestPart("password") String password,
+        @RequestPart("role") String role
+    ) {
+        try {
+            UserDTO buildUserDTO = new UserDTO();
+            buildUserDTO.setEmail(email);
+            buildUserDTO.setPassword(passwordEncoder.encode(password));
+            buildUserDTO.setRole(Role.valueOf(role));
+            authService.updateUser(buildUserDTO);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (DataPersistException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping(value = "signin",consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<JWTAuthResponse> signIn(@RequestBody SignIn signIn){
+        return ResponseEntity.ok(authService.signIn(signIn));
+    }
+    @PostMapping("refresh")
+    public ResponseEntity<JWTAuthResponse> refreshToken(@RequestParam ("existingToken") String existingToken) {
+        return ResponseEntity.ok(authService.refreshToken(existingToken));
+    }
+
 }
