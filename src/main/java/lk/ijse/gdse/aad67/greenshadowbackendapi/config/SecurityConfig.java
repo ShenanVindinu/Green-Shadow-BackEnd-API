3f6da1d01545324d92b0cd9b976016f8c5f5b7ae
ShenanVindinu <shenanvindinu@gmail.com>
2024-12-01 22:41:19 +0530

    JWT support configurations added for the API

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/config/SecurityConfig.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/config/SecurityConfig.java
@@ -4,15 +4,18 @@ import lk.ijse.gdse.aad67.greenshadowbackendapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -21,18 +24,27 @@ public class SecurityConfig {
     @Autowired
     public final UserService userService;
 
-    public SecurityConfig(UserService userService) {
+    private final JWTConfigFilter jwtConfigFilter;
+
+    public SecurityConfig(UserService userService, JWTConfigFilter jwtConfigFilter) {
         this.userService = userService;
+        this.jwtConfigFilter = jwtConfigFilter;
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(customizer -> customizer.disable())
-                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .authorizeHttpRequests(request ->
+                        request
+                                .requestMatchers("api/v1/auth/**")
+                                .permitAll()
+                                .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session ->
-                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtConfigFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
@@ -47,6 +59,11 @@ public class SecurityConfig {
         provider.setPasswordEncoder(passwordEncoder());
         provider.setUserDetailsService(userService.userDetailsService());
         return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
 
