51840dcfb20f140875882d007407e4afd7b804eb
ShenanVindinu <shenanvindinu@gmail.com>
2024-12-02 23:01:06 +0530

    service implemented to get all users

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/service/UserService.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/service/UserService.java
@@ -3,7 +3,11 @@ package lk.ijse.gdse.aad67.greenshadowbackendapi.service;
 import lk.ijse.gdse.aad67.greenshadowbackendapi.dto.UserDTO;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
+import java.util.List;
+
 public interface UserService {
     UserDetailsService userDetailsService();
     void updateUser(UserDTO buildUserDTO);
+
+    List<UserDTO> getAllusers();
 }
