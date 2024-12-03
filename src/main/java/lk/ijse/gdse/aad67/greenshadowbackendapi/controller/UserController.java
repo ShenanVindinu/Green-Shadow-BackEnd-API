10965f76b43bb1cdcf2942250202941e496f60c4
ShenanVindinu <shenanvindinu@gmail.com>
2024-12-04 00:03:43 +0530

    MANAGER PREAUTHORIZE ACCESS ADDED TO CONTROLLER

--- src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/controller/UserController.java
+++ src/main/java/lk/ijse/gdse/aad67/greenshadowbackendapi/controller/UserController.java
@@ -25,7 +25,7 @@ public class UserController {
     private final UserService userService;
     private final PasswordEncoder passwordEncoder;
 
-    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('MANAGER')")
     @PutMapping(value = "userUpdate", consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<JWTAuthResponse> updateUser(
@@ -49,7 +49,7 @@ public class UserController {
         }
     }
 
-    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('MANAGER')")
     @GetMapping(value = "getAllUsers", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<UserDTO> getAllCrops() {return userService.getAllusers();}
 
